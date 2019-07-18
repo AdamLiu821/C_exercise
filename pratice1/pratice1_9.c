09473479fc2634eb64354959cdd5ac8d3aaea937
AdamLiu821 <404411182@gms.tku.edu.tw>
2019-07-18 17:47:04 +0800

    update exercise1-22 pratice1-8 1-9

---
+++ pratice1/pratice1_9.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/*copy and transform '     ' to ' ' */
+
+int main(){
+    int c;
+    while((c = getchar()) != EOF){
+        if(c == ' '){
+            putchar(c);
+            while(c == ' '){
+                c = getchar();
+            }
+            putchar(c);
+        }
+        else{
+            putchar(c);
+        }
+    }
+}
