f37bacbaafc7884e1204968cb334a9a33607adbd
AdamLiu821 <404411182@gms.tku.edu.tw>
2019-07-22 10:32:24 +0800

    finish practice 1-17

--- ch1/practice/practice1_17.c
+++ ch1/practice/practice1_17.c
@@ -1,51 +1,38 @@
 #include<stdio.h>
 
-int copyToArrayPlusNweLine(char inputArray[], char outputArray[], int wordNumber, int point);
+#define MAXLINE 1000
+#define LOMGLINE 10
 
-int main(){
-    int c;
-    int count = 0;
-    char word[1000];
-    char output[1000];
-    int nowPoint = 0;
+int getline(char line[], int maxline);
 
-    for (int i = 0; i <= 1000; i++) {
-        output[i] = 'p';
-    }
+int main() {
+    int len;
+    char line[MAXLINE];
 
-    while ((c = getchar()) != EOF)
-    {
-        if (c != '\n')
-        {
-            word[count] = c;
-            count++;
+    while ((len = getline(line, MAXLINE)) > 0){
+        if (len > LOMGLINE) {
+            printf("%s", line);
         }
-        else
-        {
-            if (count >= 10)
-            {
-                nowPoint = copyToArrayPlusNweLine(word, output, count, nowPoint);
-            }
-            count = 0;
-        }
-    }
-
-    int j = 0;
-
-    while (output[j] != 'p') {
-        printf("%c", word[j]);
-        j++;
     }
 
     return 0;
 }
 
-int copyToArrayPlusNweLine(char inputArray[], char outputArray[], int wordLong, int startPoint){
-    for (int i = startPoint; i <= (startPoint +  wordLong); i++) {
-        outputArray[i] = inputArray[i];
+int getline(char s[], int lim) {
+    int c, i, j;
+
+    j = 0;
+    for (i = 0; (c = getchar()) != EOF && c != '\n'; i++) {
+        if (i < lim - 2) {
+            s[j] = c;
+            j++;
+        }
     }
-    int point = startPoint + wordLong;
-    outputArray[point + 1] = 'z';
-    printf("%d", point + 1);
-    return point + 1;
+    if (c == '\n') {
+        s[j] = c;
+        j++;
+        i++;
+    }
+    s[j] = '\0';
+    return i;
 }
