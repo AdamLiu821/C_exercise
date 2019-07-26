fad14a60423756cd07f79415d942feabe6d617bd
AdamLiu821 <404411182@gms.tku.edu.tw>
2019-07-26 10:05:55 +0800

    modify exercise 2-8

--- ch2/exercise/2_8.c
+++ ch2/exercise/2_8.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 
+int strlenn(char s[]);
+int strlenn(char s[]){
+    int i = 0;
+    while(s[i] != '\0'){
+        i++;
+    }
+    return i;
+}
+
 int main(){
-    printf("Hello, ""World");
+    char text[] = "hello";
+    printf("%d", strlenn(text));
 }
